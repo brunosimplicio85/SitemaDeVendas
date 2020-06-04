08d72c7d71a6fd4fbfaae43fed17bf3c5a0149fa
brunosimplicio85 <57268760+brunosimplicio85@users.noreply.github.com>
2020-06-04 14:57:57 -0300

    1

--- Sistema/src/main/java/br/com/sistema/dao/MovInt.java
+++
@@ -1,5 +0,0 @@
-package br.com.sistema.dao;
-
-public class MovInt extends GenericDAO<MovInt> {
-
-}
---
+++ Sistema/src/main/java/br/com/sistema/dao/MovIntDAO.java
@@ -0,0 +1,7 @@
+package br.com.sistema.dao;
+
+import br.com.sistema.domain.MovInt;
+
+public class MovIntDAO extends  GenericDAO<MovInt> {
+
+}
